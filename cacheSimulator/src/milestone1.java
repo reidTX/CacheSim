3753954830ad69fe3fa8d4d335fa1b7d9d09350b
ReidTX <reid.boulet@gmail.com>
2024-11-10 14:47:17 -0600

    Milestone 1 Done

--- cacheSimulator/src/milestone1.java
+++ cacheSimulator/src/milestone1.java
@@ -13,58 +13,121 @@ public class milestone1 {
 		int associativity = 0;
 		String replacement_policy = null;
 		String memory_size = null;
-		int x=0;
-		for (int i=0; i<args.length; i++) {
-			if ("-f".equals(args[i]) && i+1 < args.length) {
-				trace_file[x] = args[i+1];
-				i++;
-				x++;
-			}else if("-s".equals(args[i]) && i+1 < args.length) {
-				cache_size = Integer.parseInt(args[i+1]);
-				i++;
-			}else if("-b".equals(args[i]) && i+1 < args.length) {
-				block_size = Integer.parseInt(args[i+1]);
-				i++;
-			}else if("-a".equals(args[i]) && i+1 < args.length) {
-				associativity = Integer.parseInt(args[i+1]);
-				i++;
-			}else if("-r".equals(args[i]) && i+1 < args.length) {
-				replacement_policy = args[i+1];
-				i++;
-			}else if("-p".equals(args[i]) && i+1 < args.length) {
-				memory_size = args[i+1];
-				i++;
-			}
-		}
-		System.out.println("Cache Simulator - CS 3853 Fall 2023 - Group#2\n");
+        double percent_memory_used = 0;
+        int instructions_per_time_slice = 0;
+		int traceLen = 0;
+        int x = 0;
+        for (int i = 0; i < args.length; i++) {
+            switch (args[i]) {
+                case "-f":
+                    if (i + 1 < args.length) {
+                        trace_file[x] = args[i + 1];
+                        i++;
+                        x++;
+                    }
+                    break;
+                case "-s":
+                    if (i + 1 < args.length) {
+                        cache_size = Integer.parseInt(args[i + 1]);
+                        i++;
+                    }
+                    break;
+                case "-b":
+                    if (i + 1 < args.length) {
+                        block_size = Integer.parseInt(args[i + 1]);
+                        i++;
+                    }
+                    break;
+                case "-a":
+                    if (i + 1 < args.length) {
+                        associativity = Integer.parseInt(args[i + 1]);
+                        i++;
+                    }
+                    break;
+                case "-r":
+                    if (i + 1 < args.length) {
+                        replacement_policy = args[i + 1];
+                        i++;
+                    }
+                    break;
+                case "-p":
+                    if (i + 1 < args.length) {
+                        memory_size = args[i + 1];
+                        i++;
+                    }
+                    break;
+                case "-u":
+                    if (i + 1 < args.length) {
+                        percent_memory_used = Double.parseDouble(args[i + 1]);
+                        i++;
+                    }
+                    break;
+                case "-n":
+                    if (i + 1 < args.length) {
+                        instructions_per_time_slice = Integer.parseInt(args[i + 1]);
+                        i++;
+                    }
+                    break;
+            }
+        }
+		System.out.println("Cache Simulator - CS 3853 Fall 2024 - Group#2\n");
+		System.out.println("Trace Files:");
+        for (String file : trace_file) {
+            if (file != null) {
+                System.out.println("\t"+file);
+            }
+        }
+		System.out.println("\n***** Cache Input Parameters ******");
+		System.out.println("Cache Size: \t\t"+cache_size+" KB");
+		System.out.println("Block Size: \t\t"+block_size+" bytes");
+		System.out.println("Associativity: \t\t"+associativity);
+		System.out.println("Replacement Policy: \t"+formatReplacementPolicy(replacement_policy));
+		System.out.println("Physical Memory: \t" + memory_size);
+        System.out.println("Percent Memory Used by System: " + percent_memory_used + "%");
+        System.out.println("Instructions / Time Slice: " + instructions_per_time_slice+"\n");
+		  if(trace_file[0] != null) { 
+			traceLen++;
+			} 
+			if(trace_file[1] != null) { 
+			traceLen++;
+			} 
+			if(trace_file[2] != null) { 
+			traceLen++; 
+			} 
 		if(trace_file[0] != null) {
-			trace1(trace_file[0],cache_size,block_size,associativity,replacement_policy,memory_size);
+			processTrace(trace_file[0],cache_size,block_size,associativity,replacement_policy,memory_size,percent_memory_used,instructions_per_time_slice,traceLen);
 		}
-		if(trace_file[1] != null) {
-			trace2(trace_file[1],cache_size,block_size,associativity,replacement_policy,memory_size);
+		/*if(trace_file[1] != null) {
+			processTrace(trace_file[1],cache_size,block_size,associativity,replacement_policy,memory_size,percent_memory_used,instructions_per_time_slice,traceLen);
 		}
 		if(trace_file[2] != null) {
-			trace2(trace_file[2],cache_size,block_size,associativity,replacement_policy,memory_size);
-		}
+			processTrace(trace_file[2],cache_size,block_size,associativity,replacement_policy,memory_size,percent_memory_used,instructions_per_time_slice,traceLen);
+		}*/
 	}
-	public static void trace1(String trace_file,int cache_size,int block_size,int associativity,String replacement_policy,String memory_size) {
+		private static String formatReplacementPolicy(String replacementPolicy) {
+		switch (replacementPolicy.toLowerCase()) {
+            case "rr":
+                return "Round Robin";
+            case "lfu":
+                return "Least Frequently Used";
+            case "lru":
+                return "Least Recently Used";
+            default:
+                return replacementPolicy;
+        }
+    }
+	public static void processTrace(String trace_file,int cache_size,int block_size,int associativity,String replacement_policy,String memory_size, double percent_memory_used, int instructions_per_slice, int traceLen) {
 		int total_block = ((cache_size*1024)/block_size);
 		int rows_num = total_block / associativity;
 		int index_size = (int) (Math.log(rows_num) / Math.log(2));
 		int offset_size = (int) (Math.log(block_size)/Math.log(2));
-		int tag_size = (int) (index_size+offset_size);
-		int overhead_size = rows_num*(tag_size+1);
+		int tag_size = (int) (32-index_size-offset_size);
+		int overhead_size = (rows_num*(tag_size+1)/2);
 		int implement_memSizeb = ((cache_size*1024)+overhead_size);
 		double implement_memSizekb = implement_memSizeb / 1024;
-		double cost_perkb = 0.09;
+		double cost_perkb = 0.15;
 		double cost = Math.round((implement_memSizekb * cost_perkb)*100.0)/100.0;
-		System.out.println("Trace File: "+ trace_file+"\n");
-		System.out.println("***** Cache Input Parameters ******");
-		System.out.println("Cache Size: \t\t"+cache_size+" KB");
-		System.out.println("Block Size: \t\t"+block_size+" bytes");
-		System.out.println("Associativity: \t\t"+associativity);
-		System.out.println("Replacement Policy: \t"+replacement_policy+"\n\n");
-		System.out.println("***** Cache Calculated Values *****");
+		System.out.println("***** Cache Calculated Values *****\n");
 		System.out.println("Total # Blocks: \t\t"+total_block);
 		System.out.println("Tag Size: \t\t\t"+tag_size+" bits");
 		System.out.println("Index Size: \t\t\t"+index_size+" bits");
@@ -72,76 +135,26 @@ public class milestone1 {
 		System.out.println("Overhead Size: \t\t\t"+overhead_size+" bytes");
 		System.out.println("Implementation Memory Size: \t"+implement_memSizekb+" KB ("+implement_memSizeb+" bytes)");
 		System.out.println("Cost: \t\t\t\t$"+cost+" @ ($"+cost_perkb+" / KB)");
-		fileRead(trace_file);
-		System.out.println("\n\n");
-	}
-	public static void trace2(String trace_file,int cache_size,int block_size,int associativity,String replacement_policy,String memory_size) {
-		int total_block = ((cache_size*1024)/block_size);
-		int rows_num = total_block / associativity;
-		int index_size = (int) (Math.log(rows_num) / Math.log(2));
-		int offset_size = (int) (Math.log(block_size)/Math.log(2));
-		int tag_size = (int) (index_size+offset_size);
-		int overhead_size = rows_num*(tag_size+1);
-		int implement_memSizeb = ((cache_size*1024)+overhead_size);
-		double implement_memSizekb = implement_memSizeb / 1024;
-		double cost_perkb = 0.09;
-		double cost = Math.round((implement_memSizekb * cost_perkb)*100.0)/100.0;
-		System.out.println("Trace File: "+ trace_file+"\n");
-		System.out.println("***** Cache Input Parameters ******");
-		System.out.println("Cache Size: \t\t"+cache_size+" KB");
-		System.out.println("Block Size: \t\t"+block_size+" bytes");
-		System.out.println("Associativity: \t\t"+associativity);
-		System.out.println("Replacement Policy: \t"+replacement_policy+"\n\n");
-		System.out.println("***** Cache Calculated Values *****");
-		System.out.println("Total # Blocks: \t\t"+total_block);
-		System.out.println("Tag Size: \t\t\t"+tag_size+" bits");
-		System.out.println("Index Size: \t\t\t"+index_size+" bits");
-		System.out.println("Total # Rows: \t\t\t"+rows_num);
-		System.out.println("Overhead Size: \t\t\t"+overhead_size+" bytes");
-		System.out.println("Implementation Memory Size: \t"+implement_memSizekb+" KB ("+implement_memSizeb+" bytes)");
-		System.out.println("Cost: \t\t\t\t$"+cost+" @ ($"+cost_perkb+" / KB)");
-		fileRead(trace_file);
-		System.out.println("\n\n");
-	}
-	public static void trace3(String trace_file,int cache_size,int block_size,int associativity,String replacement_policy,String memory_size) {
-		int total_block = ((cache_size*1024)/block_size);
-		int rows_num = total_block / associativity;
-		int index_size = (int) (Math.log(rows_num) / Math.log(2));
-		int offset_size = (int) (Math.log(block_size)/Math.log(2));
-		int tag_size = (int) (index_size+offset_size);
-		int overhead_size = rows_num*(tag_size+1);
-		int implement_memSizeb = ((cache_size*1024)+overhead_size);
-		double implement_memSizekb = implement_memSizeb / 1024;
-		double cost_perkb = 0.09;
-		double cost = Math.round((implement_memSizekb * cost_perkb)*100.0)/100.0;
-		System.out.println("Trace File: "+ trace_file+"\n");
-		System.out.println("***** Cache Input Parameters ******");
-		System.out.println("Cache Size: \t\t"+cache_size+" KB");
-		System.out.println("Block Size: \t\t"+block_size+" bytes");
-		System.out.println("Associativity: \t\t"+associativity);
-		System.out.println("Replacement Policy: \t"+replacement_policy+"\n\n");
-		System.out.println("***** Cache Calculated Values *****");
-		System.out.println("Total # Blocks: \t\t"+total_block);
-		System.out.println("Tag Size: \t\t\t"+tag_size+" bits");
-		System.out.println("Index Size: \t\t\t"+index_size+" bits");
-		System.out.println("Total # Rows: \t\t\t"+rows_num);
-		System.out.println("Overhead Size: \t\t\t"+overhead_size+" bytes");
-		System.out.println("Implementation Memory Size: \t"+implement_memSizekb+" KB ("+implement_memSizeb+" bytes)");
-		System.out.println("Cost: \t\t\t\t$"+cost+" @ ($"+cost_perkb+" / KB)");
-		fileRead(trace_file);
-		System.out.println("\n\n");
+		int physical_pages = (Integer.parseInt(memory_size)* 1024*1024) / (4*1024);
+  		int system_pages = (int) (physical_pages * (percent_memory_used/100.0));
+  		int entry_bits=(int) (Math.log(physical_pages)/Math.log(2)) + 1;
+  		int total_ram = (512*1024*traceLen*entry_bits/8);
+		System.out.println("\n***** Physical Memory Calculated Values *****\n");
+		System.out.println("Number of Physical Pages: \t" + physical_pages);
+		System.out.println("Number of Pages for System: \t" + system_pages);
+		System.out.println("Size of Page Table Entry: \t" + entry_bits + " bits");
+		System.out.println("Total RAM for Page Table(s): \t" + total_ram + " bytes\n");
 	}
 	public static int fileRead(String fileName) {
 		try {
-			String fileInput = fileName;
+			BufferedReader br = new BufferedReader(new FileReader(fileName));
 			int i=0;
-			BufferedReader br = new BufferedReader(new FileReader(fileInput));
             String line;
             Pattern pattern = Pattern.compile("EIP \\((\\d+)\\): ([0-9a-fA-F]+)");
             while ((line = br.readLine()) != null) {
                 Matcher matcher = pattern.matcher(line);
                 if (matcher.find()) {
-                    String length = 000+matcher.group(1);
+                    String length = "000"+matcher.group(1);
                     String hexValue = matcher.group(2);
                     System.out.println("0x" + hexValue+":("+length+")");
                     i++;
